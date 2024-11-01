90f94fa787d3c1947ae60b2cc43c4bd2423882ad
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-11-01 03:13:08 -0300

    refactor: Applying uow to dependencies.

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 
@@ -13,6 +14,7 @@ import (
 	"github.com/wallison/fc-ms-wallet/internal/web"
 	"github.com/wallison/fc-ms-wallet/internal/web/webserver"
 	"github.com/wallison/fc-ms-wallet/pkg/events"
+	"github.com/wallison/fc-ms-wallet/pkg/uow"
 )
 
 func main() {
@@ -27,11 +29,21 @@ func main() {
 	// eventDispatcher.Register("TransactionCreated", handler)
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
-	transactionDb := database.NewTransactionDB(db)
+
+	ctx := context.Background()
+	uow := uow.NewUow(ctx, db)
+
+	uow.Register("AccountDB", func(tx *sql.Tx) interface{} {
+		return database.NewAccountDB(db)
+	})
+
+	uow.Register("TransactionDB", func(tx *sql.Tx) interface{} {
+		return database.NewTransactionDB(db)
+	})
 
 	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
 	createaccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
-	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent)
 
 	webserver := webserver.NewWebServer(":3000")
 
--- internal/web/transaction_handler.go
+++ internal/web/transaction_handler.go
@@ -25,7 +25,8 @@ func (h *WebTransactionHandler) CreateTransaction(w http.ResponseWriter, r *http
 		return
 	}
 
-	output, err := h.CreateTransactionUseCase.Execute(dto)
+	ctx := r.Context()
+	output, err := h.CreateTransactionUseCase.Execute(ctx, dto)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
