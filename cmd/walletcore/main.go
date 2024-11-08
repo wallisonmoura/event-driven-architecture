0c9b93c996b046d6896d69c7a0c86e8f81e0efe7
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-11-08 16:45:53 -0300

    feat: register handler with kafka

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -5,28 +5,38 @@ import (
 	"database/sql"
 	"fmt"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/wallison/fc-ms-wallet/internal/database"
 	"github.com/wallison/fc-ms-wallet/internal/event"
+	"github.com/wallison/fc-ms-wallet/internal/event/handler"
 	"github.com/wallison/fc-ms-wallet/internal/usecase/create_account"
 	"github.com/wallison/fc-ms-wallet/internal/usecase/create_client"
 	"github.com/wallison/fc-ms-wallet/internal/usecase/create_transaction"
 	"github.com/wallison/fc-ms-wallet/internal/web"
 	"github.com/wallison/fc-ms-wallet/internal/web/webserver"
 	"github.com/wallison/fc-ms-wallet/pkg/events"
+	"github.com/wallison/fc-ms-wallet/pkg/kafka"
 	"github.com/wallison/fc-ms-wallet/pkg/uow"
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	transactionCreatedEvent := event.NewTransactionCreated()
-	// eventDispatcher.Register("TransactionCreated", handler)
+
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
 
@@ -45,7 +55,7 @@ func main() {
 	createaccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
 	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent)
 
-	webserver := webserver.NewWebServer(":3000")
+	webserver := webserver.NewWebServer(":8080")
 
 	clientHandler := web.NewWebClientHandler(*createClientUseCase)
 	accountHandler := web.NewWebAccountHandler(*createaccountUseCase)
@@ -55,5 +65,6 @@ func main() {
 	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
 	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
+	fmt.Println("Server is running")
 	webserver.Start()
 }
